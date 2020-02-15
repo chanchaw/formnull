bb667bef4e18f7f6b6d4eb78c7fac0d8148886a0
chanchaw <chan_chaw@126.com>
2020-02-15 13:30:14 +0800

    修改为首页显示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first';
+
+  loginForm:FormGroup;
+
+  @ViewChild("userid",{static:false})
+  private userid_er:ElementRef
+
+  private userid$ = new Subject();
+
+  constructor(
+    private fb:FormBuilder
+  ) { 
+    this.initLoginForm();// 初始化登录表单
+  }
+
+  ngOnInit() {
+  }
+
+  initLoginForm(){
+    this.loginForm = this.fb.group({
+      userid:[null,Validators.required],
+      userName:[{value:null,disabled:true},Validators.required],
+      password:[null,Validators.required]
+    });
+
+  }
+
+
+  // 登录
+  login(){
+    this.log('表单数据是：');
+    console.log(this.loginForm.value);
+    
+    const userId:string = this.loginForm.controls.userid.value;
+    const pw:string = this.loginForm.controls.password.value
+    console.log('即将发送给服务的两个参数依次是：' + userId + ',' + pw);
+
+  }
+
+
+
+  log(content:string){
+    console.log(content);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,MatInputModule,MatButtonModule,FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
